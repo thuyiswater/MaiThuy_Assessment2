809de65d8a7d24cf27e3bdec13e0e6e3c4f73f1b
thuyiswater <87476877+thuyiswater@users.noreply.github.com>
2023-08-15 15:58:14 +0700

    Update function.h

--- src/function.h
+++ src/function.h
@@ -1,8 +1,12 @@
 #ifndef SRC_FUNCTION_H_
 #define SRC_FUNCTION_H_
 
+char color[8][10] = {"black", "red", "green", "yellow", "blue", "purple", "cyan", "white"};
+void reset(char *ar);
 void welcome();
 void compare_cli();
 void execute_command();
+int compare_color(char *str1, char *str2);
+int get_color(char *str1);
 
 #endif
