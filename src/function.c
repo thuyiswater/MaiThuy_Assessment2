895751c2b9b8408799745aa72a51ae4b91662991
thuyiswater <87476877+thuyiswater@users.noreply.github.com>
2023-08-15 15:57:59 +0700

    Update function.c

--- src/function.c
+++ src/function.c
@@ -18,7 +18,12 @@ void welcome() {
     uart_puts("Y8888P' YP   YP 88   YD Y88888P    `Y88P'  `8888Y'\n");
 
     uart_puts("\tDeveloped by Thuy - s3877746\n\n");
-    uart_puts("MyOS>");
+}
+
+void reset(char* ar) {
+    while (*ar != '\n'){
+        ar++;
+    }
 }
 
 int compare_cli(char* str1, char* str2) {
@@ -32,6 +37,7 @@ int compare_cli(char* str1, char* str2) {
             break;
         }
     }
+    reset(str1);
     return flag;
 }
 
@@ -48,4 +54,58 @@ void execute_command() {
     uart_puts("-t <text color> -b <background color>\n");
     uart_puts("showinfo                  - Show board revision and board MAC address in correct format/ meaningful information\n");
     uart_puts("                          - Example: MyOS> showinfo");
+}
+
+int compare_color(char* str1, char* str2) {
+    int i = 0;
+    int flag = 0;
+    while (*s2 != '\0') {
+        if (*str1 == *str2) {
+            i++; str1++; str2++;
+        } else {
+            flag = 1;
+            str1 = str1 - i;
+            break;
+        }
+    }
+    return flag;
+}
+
+int get_colorset(char* str1) {
+    str1++;
+    int flag = 0;
+
+    if(*str1 == '\n') {
+        flag = -1;
+        reurn flag;
+    }
+
+    if (*str1 == ' '){
+        str1++;
+        int color = -1;
+
+        for(int i = 0; i <= 8; i++) {
+            if (compare_color(s1,"black") == 0){
+                color = 0;
+            } else if(compare_color(s1, "red") == 0) {
+                color = 1;
+            } else if (compare_color(s1, "green") == 0) {
+                color = 2;
+            } else if (compare_color(s1, "yellow") == 0) {
+                color = 3;
+            } else if (compare_color(s1, "blue") == 0) {
+                color = 4;
+            } else if (compare_color(s1, "purple") == 0) {
+                color = 5;
+            } else if(compare_color(s1, "cyan") == 0) {
+                color = 6;
+            } else of(compare_color(s1, "white") == 0) {
+                color = 7;
+            }
+        }
+        return color;
+    } else {
+        flag = -1;
+        return flag;
+    }
 }
